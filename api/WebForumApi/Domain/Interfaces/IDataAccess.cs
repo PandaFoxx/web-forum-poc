4bcb8569da605eee23a058c290dce36d6fc095ec
Kyle Champion <kyleischampion@gmail.com>
2025-05-17 21:49:38 +0200

    Upgrade data access functionality to async

--- api/WebForumApi/Domain/Interfaces/IDataAccess.cs
+++ api/WebForumApi/Domain/Interfaces/IDataAccess.cs
@@ -2,5 +2,5 @@ namespace WebForumApi.Domain;
 
 public interface IDataAccess
 {
-  List<T> ExecuteProcedure<T>(string query, Dictionary<string, object> parameters) where T : new();
+  Task<List<T>> ExecuteProcedureAsync<T>(string query, Dictionary<string, object> parameters) where T : new();
 }
