bece606d10bade9ec5bd1b37b25828cea701f0ab
Kyle Champion <kyleischampion@gmail.com>
2025-05-17 11:46:05 +0200

    Integrate unit test project with API

--- api/WebForumApi/Controllers/UserController.cs
+++ api/WebForumApi/Controllers/UserController.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using WebForumApi.Application.User.Login;
 
 namespace WebForumApi.Controllers;
 
+[ExcludeFromCodeCoverage]
 [ApiController]
 [Route("api/[controller]")]
 public sealed class UserController(IMediator mediator)
--- api/WebForumApi/Program.cs
+++ api/WebForumApi/Program.cs
@@ -1,14 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Diagnostics.CodeAnalysis;
 
-builder.Services.AddMediatR(config =>
-  config.RegisterServicesFromAssembly(typeof(Program).Assembly)
-);
+[ExcludeFromCodeCoverage]
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+    builder.Services.AddMediatR(config =>
+      config.RegisterServicesFromAssembly(typeof(Program).Assembly)
+    );
 
-var app = builder.Build();
-app.UseRouting();
-app.UseHttpsRedirection();
-app.MapControllers();
-app.Run();
+    builder.Services.AddControllers();
 
+    var app = builder.Build();
+    app.UseRouting();
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+  }
+}
