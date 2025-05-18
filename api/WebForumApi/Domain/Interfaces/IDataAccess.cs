a79ecbf9c076cb92840c240223dca80a3ce33a5a
Kyle Champion <kyleischampion@gmail.com>
2025-05-18 19:57:53 +0200

    Add function to execute procedure without returning data

--- api/WebForumApi/Domain/Interfaces/IDataAccess.cs
+++ api/WebForumApi/Domain/Interfaces/IDataAccess.cs
@@ -3,4 +3,6 @@ namespace WebForumApi.Domain;
 public interface IDataAccess
 {
   Task<List<T>> ExecuteProcedureAsync<T>(string query, Dictionary<string, object> parameters) where T : new();
+
+  Task ExecuteProcedureAsync(string query, Dictionary<string, object> parameters);
 }
